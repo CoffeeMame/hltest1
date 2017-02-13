69aa5dd074c70e6ca657b02fe929ff87651930d2
Yoshihiro Ueda <yoshiroueda@ictlab-no-MacBook-Pro.local>
2017-02-13 19:54:08 +0900

    no message

--- hltest.go
+++ hltest.go
@@ -237,7 +237,7 @@ func (t *SimpleChaincode) create_baggage(stub shim.ChaincodeStubInterface, args 
 //=============================================================================================================================
 func (t *SimpleChaincode) delete_baggage(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var err error
-	var indexslice []string
+	var indexslice []byte
 
 	// 引数の数をチェック
 	if len(args) != 1 {
@@ -256,9 +256,9 @@ func (t *SimpleChaincode) delete_baggage(stub shim.ChaincodeStubInterface, args 
 
 	// IDのスライスから削除
 	indexslice, err = stub.GetState(BAGGAGE_INDEX_STR)
-	s := make([]string, len(indexslice) - 1 )
+	s := make([]byte, len(indexslice) - 1 )
 	for i := 0; i < len(indexslice); i++ {
-		if indexslice[i] == args[0] {
+		if string(indexslice[i]) == args[0] {
 			s = append(s, indexslice[i])
 		}
 	}
