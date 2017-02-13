a964c698a651ae9fc56a28e97099eabe8be2a2a0
Yoshihiro Ueda <yoshiroueda@ictlab-no-MacBook-Pro.local>
2017-02-13 13:45:20 +0900

    no message

--- hltest.go
+++ hltest.go
@@ -322,8 +322,8 @@ func (t *SimpleChaincode) change_state(stub shim.ChaincodeStubInterface, args []
 		return nil, errors.New("5th argument must be a non-empty string")
 	}
 	id = args[0]
-	tempVal, err := strconv.Atoi(args[1])
-	humVal, err := strconv.Atoi(args[2])
+	tempVal, err = strconv.Atoi(args[1])
+	humVal, err = strconv.Atoi(args[2])
 	prestate = args[3]
 	poststate = args[4]
 	// 存在チェック
@@ -367,7 +367,7 @@ func (t *SimpleChaincode) change_state(stub shim.ChaincodeStubInterface, args []
 	}
 	*/
 	if tempVal < humVal {
-		// 
+		//
 	}
 
 	// 状態を更新
